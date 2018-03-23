b912b817b241b09fcb9ad8c7e76b19b63e7ea4e7
Deak Levente <Dlevi93@outlook.com>
2018-03-23 14:21:26 +0200

    FixVt

--- src/app/components/registrationflow/result/result.component.ts
+++ src/app/components/registrationflow/result/result.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 
 import { FormData } from '../data/formData.model';
 import { FormDataService } from '../data/formData.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
     // tslint:disable-next-line:component-selector
@@ -13,8 +14,10 @@ export class ResultComponent implements OnInit {
     title = 'Összegzés';
     @Input() formData: FormData;
     isFormValid = false;
+    _http: HttpClient;
 
-    constructor(private formDataService: FormDataService) {
+    constructor(private formDataService: FormDataService, http: HttpClient) {
+        this._http = http;
     }
 
     ngOnInit() {
@@ -25,7 +28,10 @@ export class ResultComponent implements OnInit {
 
     submit() {
         alert('Excellent Job!');
-        this.formData = this.formDataService.resetFormData();
+
+        console.log(this.formData);
+        this._http.post('http://localhost:49223/api/values/', this.formData).subscribe(res => console.log(res));
+        // this.formData = this.formDataService.resetFormData();
         this.isFormValid = false;
     }
 }
